4ca9893584a34d68ab0c02905ecb83f1ef6de853
mnishimori <mnishimori@yahoo.com.br>
2022-06-10 23:08:00 -0300

    Asserções assíncronas e o uso do Spy

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -21,20 +21,28 @@ describe(LikeWidgetComponent.name, () => {
     component = fixture.componentInstance;
   });
 
-  it('should create', () => {
+  it('Should create component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should auto generate ID when input property is missing', () => {
+  it('Should auto-generate ID during ngOnInit when (@Input id) is not assigned', () => {
     fixture.detectChanges();
     expect(component.id).toBeTruthy();
   })
 
-  it('Should not generate ID when input property is present', () => {
+  it('Should not auto-generate ID during ngOnInit when (@Input id) is assigned', () => {
     const someId = 'someId';
     component.id = someId;
     fixture.detectChanges();
     expect(component.id).toEqual(someId);
+  })
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    Should trigger (@Output liked) when called`, () => {
+      spyOn(component.liked, 'emit');
+      fixture.detectChanges();
+      component.like();
+      expect(component.liked.emit).toHaveBeenCalled();
   })
 
 });
