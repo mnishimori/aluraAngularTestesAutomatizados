c8ef1fe71e29adee2749e193eafbfcd748d2ea2f
mnishimori <mnishimori@yahoo.com.br>
2022-06-14 09:09:47 -0300

    Testes do componente com integração ao elemento do DOM

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,4 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LikeWidgetModule } from './../like-widget/like-widget.module';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { PhotoFrameComponent } from './photo-frame.component';
 
@@ -8,7 +9,8 @@ describe(PhotoFrameComponent.name, () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PhotoFrameComponent ]
+      declarations: [ PhotoFrameComponent ],
+      imports: [ LikeWidgetModule ]
     })
     .compileComponents();
   });
@@ -21,4 +23,37 @@ describe(PhotoFrameComponent.name, () => {
   it('should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    should trigger (@Output liked) once when called multiple times
+    within debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+    let times = 0;
+    component.liked.subscribe(() => times++);
+    component.like();
+    component.like();
+    tick(500);
+    expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    should trigger (@Output liked) two times when
+    called outside debounce time`, fakeAsync(() => {
+      fixture.detectChanges();
+      let times = 0;
+      component.liked.subscribe(() => times++);
+      component.like();
+      tick(500);
+      component.like();
+      tick(500);
+      expect(times).toBe(2);
+    }))
+
+    it(`Should display number of likes when (@Input likes) is incremented`, () => {
+      fixture.detectChanges();
+      component.likes++;
+      fixture.detectChanges();
+      const element: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+      expect(element.textContent.trim()).toBe('1');
+    })
 });
